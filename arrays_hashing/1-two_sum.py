f056a0b9cc4d2adca61fd9cef06078bba276c3da
Pol Pastells <polpastells@gmail.com>
2023-12-16 21:54:02 +0100

    1

---
+++ arrays_hashing/1-two_sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i, num in enumerate(nums):
+            diff = target-num
+            if diff in hash:
+                return [i, hash[diff]]
+            else:
+                hash[num] = i
+
+        return [-1, -1]
